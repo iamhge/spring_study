b3a6fdc7cf4fd3af595c8f393797358cdac52213
iamhge <fine632@gmail.com>
2021-04-03 21:51:00 +0900

    API

--- spring_intro/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ spring_intro/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -26,5 +27,40 @@ public class HelloController {
     public String helloMvc(@RequestParam(name = "name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    // html이 아닌, 그대로 데이터를 넣어줌.
+    // http://localhost:8080/hello-string?name=spring
+    // 위와 같이 쳐서 들어간 후 page source를 보면 html tag가 아닌 문자 내용을 직접 반환한 것 확인 가능
+    @GetMapping("hello-string")
+    // @ResponseBody : http에서 응답 body부의 데이터를 직접 넣어주겠다.
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // ex) name을 spring이라 넣으면 -> "hello spring"
+    }
+
+    @GetMapping("hello-api")
+    // 이전에는 viewResolver가 동작했으나, @ResponseBody 때문에 HttpMessageConverter를 이용
+    // 이때, 객체이므로 JsonConverter가 동작. (cf. 단순 문자열의 경우 StringConverter 동작)
+    @ResponseBody
+    // 객체를 return 해야 함.
+    // (default) json 방식으로 데이터를 만들어서 http에 반환함.
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        // property 접근 방식 (getter, setter)
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
