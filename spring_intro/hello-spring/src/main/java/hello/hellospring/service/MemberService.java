9630000e3377bfaa3c0b5b1b0d0902f97b5f1b27
iamhge <fine632@gmail.com>
2021-04-10 00:15:17 +0900

    when we need to use AOP

--- spring_intro/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ spring_intro/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -54,13 +54,20 @@ public class MemberService {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 })
         */
+        long start = System.currentTimeMillis();
 
-        // 방법 3) extract method
-        validateDuplicateMember(member);
+        try {
+            // 방법 3) extract method
+            validateDuplicateMember(member);
 
-        // 중복 회원 없다면 save
-        memberRepository.save(member);
-        return member.getId();
+            // 중복 회원 없다면 save
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -72,7 +79,14 @@ public class MemberService {
 
     /* 전체 회원 조회 */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
