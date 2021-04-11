a9c15afdc2372c66a48c535b909c109a1ea0ad27
iamhge <fine632@gmail.com>
2021-04-11 22:58:11 +0900

    project hw

--- spring_intro_ex/spring-petclinic-main/src/main/java/org/springframework/samples/petclinic/owner/Owner.java
+++ spring_intro_ex/spring-petclinic-main/src/main/java/org/springframework/samples/petclinic/owner/Owner.java
@@ -59,6 +59,9 @@ public class Owner extends Person {
 	@Digits(fraction = 0, integer = 10)
 	private String telephone;
 
+	@Column(name = "age")
+	private Integer age;
+
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
 	private Set<Pet> pets;
 
@@ -84,6 +87,14 @@ public class Owner extends Person {
 
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
 	}
 
 	protected Set<Pet> getPetsInternal() {
