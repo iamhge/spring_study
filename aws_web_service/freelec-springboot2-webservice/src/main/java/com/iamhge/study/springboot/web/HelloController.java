df24c8a16ac014033094865244226f5a8b6045af
iamhge <fine632@gmail.com>
2021-06-08 01:42:54 +0900

    2.4 Hello Controller 코드를 롬복으로 전환하기

--- aws_web_service/freelec-springboot2-webservice/src/main/java/com/iamhge/study/springboot/web/HelloController.java
+++ aws_web_service/freelec-springboot2-webservice/src/main/java/com/iamhge/study/springboot/web/HelloController.java
@@ -1,7 +1,9 @@
 // p60
 package com.iamhge.study.springboot.web;
 
+import com.iamhge.study.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // @RestController
@@ -9,6 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 // * 예전에는 @ResponseBody를 각 메소드마다 선언했던 것을 한번에 사용할 수 있게 해주는 것.
 @RestController
 public class HelloController {
+    // p75
+    @GetMapping("/hello/dto")
+    // @RequestParam : 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+
     // @GetMapping
     // * HTTP 메소드인 GET의 요청을 받을 수 있는 API를 만들어 준다.
     // * 예전에는 @RequestMapping(method = RequestMethod.GET)으로 사용되었다.
--- aws_web_service/freelec-springboot2-webservice/src/test/java/com/iamhge/study/springboot/web/HelloControllerTest.java
+++ aws_web_service/freelec-springboot2-webservice/src/test/java/com/iamhge/study/springboot/web/HelloControllerTest.java
@@ -8,9 +8,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)// 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자 실행
 @WebMvcTest(controllers = HelloController.class) // Web(Spring Mvc)에 집중할 수 있는 어노테이션
@@ -27,6 +27,25 @@ public class HelloControllerTest {
                 .andExpect(status().isOk()) // 결과 검증. Http Header의 Status가 200인지 아닌지
                 .andExpect(content().string(hello)); // 결과(응답 본문) 검증. Controller의 return 값이 "hello"이므로, 이 값이 맞는지
     }
+
+    // p75
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        // param : API 테스트 시 사용될 요청 파라미터 설정. 값은 String만 허용
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                // jsonPath : JSON 응답값을 필드별로 검증할 수 있는 메소드
+                // $를 기준으로 필드명 명시
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+
 }
 
 // test 실행 안될 경우 intellij의 runner 변경.
