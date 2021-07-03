314a40d480c8ce7d1e77b31328c96edaeb9b3fc4
iamhge <fine632@gmail.com>
2021-07-04 02:23:05 +0900

    5.6 네이버 로그인

--- aws_web_service/freelec-springboot2-webservice/src/main/java/com/iamhge/study/springboot/config/auth/dto/OAuthAttributes.java
+++ aws_web_service/freelec-springboot2-webservice/src/main/java/com/iamhge/study/springboot/config/auth/dto/OAuthAttributes.java
@@ -1,10 +1,11 @@
-// p185
+// p185, p208
 package com.iamhge.study.springboot.config.auth.dto;
 
 import com.iamhge.study.springboot.domain.user.Role;
 import com.iamhge.study.springboot.domain.user.User;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.security.core.parameters.P;
 
 import java.util.Map;
 
@@ -28,6 +29,10 @@ public class OAuthAttributes {
     // of()
     // * OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환해야만 한다.
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        // 네이버인지 판단
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -41,6 +46,19 @@ public class OAuthAttributes {
                 .build();
     }
 
+    // 네이버 생성자
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+
+    }
     // toEntity()
     // * User 엔티티를 생성한다.
     // * OAuthAttributes에서 엔티티를 생성하는 시점은 처음 가입할 때이다.
